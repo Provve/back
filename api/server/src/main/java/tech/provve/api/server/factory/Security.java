58b0f8942af5dc88ee0768b245c1d50f9ec5b347
Алексей Видеман <41009944+AlekseyVideman@users.noreply.github.com>
2026-02-08 21:42:00 +0700

    Реализовал бекенд-часть перенаправления на аутентификацию. (#11)

--- api/server/src/main/java/tech/provve/api/server/factory/Security.java
+++ api/server/src/main/java/tech/provve/api/server/factory/Security.java
@@ -8,13 +8,34 @@ import io.vertx.core.Vertx;
 import io.vertx.ext.auth.PubSecKeyOptions;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTAuthOptions;
+import io.vertx.ext.web.handler.JWTAuthHandler;
 import jakarta.inject.Named;
 
 @Factory
 public class Security {
 
+    public static final String JWT_PROVIDER_AUTH = "auth";
+
+    public static final String JWT_HANDLER_AUTH = "auth-handler";
+
+    public static final String JWT_PROVIDER_RESET = "reset";
+
+    public static final String JWT_HANDLER_RESET = "reset-handler";
+
     @Bean
-    @Named("auth")
+    @Named(JWT_HANDLER_AUTH)
+    public JWTAuthHandler jwtAuthHandler(@Named(JWT_PROVIDER_AUTH) JWTAuth jwtAuth) {
+        return JWTAuthHandler.create(jwtAuth);
+    }
+
+    @Bean
+    @Named(JWT_HANDLER_RESET)
+    public JWTAuthHandler jwtResetHandler(@Named(JWT_PROVIDER_RESET) JWTAuth jwtReset) {
+        return JWTAuthHandler.create(jwtReset);
+    }
+
+    @Bean
+    @Named(JWT_PROVIDER_AUTH)
     public JWTAuth jwtAuth(@External Vertx vertx) {
         var options = new JWTAuthOptions()
                 .addPubSecKey(new PubSecKeyOptions()
@@ -25,7 +46,7 @@ public class Security {
     }
 
     @Bean
-    @Named("reset")
+    @Named(JWT_PROVIDER_RESET)
     public JWTAuth jwtReset(@External Vertx vertx) {
         var options = new JWTAuthOptions()
                 .addPubSecKey(new PubSecKeyOptions()
