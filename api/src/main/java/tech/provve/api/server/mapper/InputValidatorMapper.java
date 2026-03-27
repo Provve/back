fe6aef7aa0ac923e4e9a0a83c69d7533d0315bcc
Aleksey Videman <alekseyvideman@gmail.com>
2026-03-27 12:51:39 +0700

    Добавил ограничение на Pagination, чтобы не было NPE

--- api/src/main/java/tech/provve/api/server/mapper/InputValidatorMapper.java
+++ api/src/main/java/tech/provve/api/server/mapper/InputValidatorMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 import tech.provve.api.server.generated.dto.Filter;
+import tech.provve.api.server.generated.dto.Pagination;
 import tech.provve.api.server.validation.dto.*;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.ERROR)
@@ -33,8 +34,12 @@ public interface InputValidatorMapper {
 
     CollectionRequest.Filter map(Filter from);
 
+    CollectionRequest.Pagination map(Pagination from);
+
     default CollectionAuthenticatedRequest map(tech.provve.api.server.generated.dto.CollectionAuthenticatedRequest from) {
-        return new CollectionAuthenticatedRequest(new CollectionRequest(map(from.getFilter())), from.getAuthToken());
+        return new CollectionAuthenticatedRequest(new CollectionRequest(map(from.getFilter()),
+                                                                        map(from.getPagination())),
+                                                  from.getAuthToken());
     }
 
 }
